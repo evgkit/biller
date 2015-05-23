16f195483b87fb721b644ecbf3ddad67104b7e8f
evgkit <kitaev.ea@gmail.com>
2015-05-23 10:33:47 +0300

    Add few scopes in Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < Account
   has_many :time_entries
+
+  scope :premier, -> { where("employees > ?", 55) }
+  scope :newest, -> { order("created_at DESC") }
+  scope :start_with, -> (letter) { where("name LIKE ?", letter + "%") }
 end
 
