6b9015cc84fe5a6b1c6c9d0f8e28160a9dafa404
evgkit <kitaev.ea@gmail.com>
2015-05-23 08:03:28 +0300

    Add some validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,4 @@
 class Customer < Account
   has_many :time_entries
-
-  #@customer.time_entries #only TimeEntries
-  #@customer.account_entries #a mix of TimeEntries and AccountEntries
 end
 
--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,6 +1,8 @@
 class TimeEntry < AccountEntry
   belongs_to :employee
 
+  validates :time, numericality: {greater_than: 0, less_than: 100}
+
   def cost
     time * 34
   end
